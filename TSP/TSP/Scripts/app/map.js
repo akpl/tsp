a73912001b14c82df7d65b83548592f22d50012d
akpl <akpl@krzeszowski.net>
2016-10-27 19:13:56 +0200

    Google Maps API key

---
+++ TSP/TSP/Scripts/app/map.js
@@ -0,0 +1,17 @@
+﻿function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 50.061820, lng: 19.936709 },
+        zoom: 13
+    });
+
+    var locationSearchInput = document.getElementById('location-input');
+    var autocomplete = new google.maps.places.Autocomplete(locationSearchInput);
+    autocomplete.bindTo('bounds', map);
+    autocomplete.addListener('place_changed', function () {
+        var place = autocomplete.getPlace();
+        var marker = new google.maps.Marker({
+            map: map,
+            position: place.geometry.location
+        });
+    });
+}
